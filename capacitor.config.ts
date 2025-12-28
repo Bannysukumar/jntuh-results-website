e729f29f95b2f50b95e5909a024c75c5d4009a34
munna <munnacys@gmail.com>
2025-12-28 15:46:27 +0530

    Update package name to com.manajntuhresults.mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.manajntuhresults.app',
+  appId: 'com.manajntuhresults.mobile',
   appName: 'Mana JNTUH Results',
   webDir: 'out',
   server: {
