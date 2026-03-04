b2d0f0dea1464edee5d3ef882a771e0cc4674426
Your Name <you@example.com>
2026-03-04 15:57:33 +0530

    Revert "Apply selected upstream fixes for notifications, syllabus, caching, and result links."
    This reverts commit b54dc0c2322271ee4372d0634babddc12498b187.

--- components/googledocviewer/GoogleDocViewer.tsx
+++ components/googledocviewer/GoogleDocViewer.tsx
@@ -12,7 +12,7 @@ const GoogleDocViewer: React.FC<GoogleDocViewerProps> = ({
   url,
   splNote,
   width = "100%",
-  height = "92vh",
+  height = "580px",
 }) => {
   const src = `https://docs.google.com/viewer?url=${encodeURIComponent(url)}&embedded=true`;
   const [isLoading, setIsLoading] = useState(true);
@@ -25,15 +25,15 @@ const GoogleDocViewer: React.FC<GoogleDocViewerProps> = ({
     }
   }, []);
   return (
-    <div>
+    <>
       {isLoading && <Loading splNote={splNote} />}
       <iframe
         id="doc-iframe"
         src={src}
-        style={{ width, height, display: isLoading ? "none" : "block" }}
+        style={{ width, height }}
         frameBorder="0"
       ></iframe>
-    </div>
+    </>
   );
 };
 
